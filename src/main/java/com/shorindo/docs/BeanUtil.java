fc21d5a6cbde2a000d6469504d43d4ff914ba485
Kazuyuki Matsuda <kazm@shorindo.com>
2018-03-11 13:10:28 +0900

    BeanUtilの階層構造対応。

--- src/main/java/com/shorindo/docs/BeanUtil.java
+++ src/main/java/com/shorindo/docs/BeanUtil.java
@@ -15,7 +15,10 @@
  */
 package com.shorindo.docs;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,22 +29,34 @@ import java.util.regex.Pattern;
 public class BeanUtil {
     private static final ActionLogger LOG = ActionLogger.getLogger(BeanUtil.class);
     private static final Pattern propPattern = Pattern.compile("([a-zA-Z])([a-z0-9]*)");
-//    private static Pattern SNAKE_PATTERN = Pattern.compile("_*([^_])([^_]*)");
-//
-//    private static String snake2camel(String name) {
-//        Matcher m = SNAKE_PATTERN.matcher(name);
-//        StringBuilder sb = new StringBuilder();
-//        int start = 0;
-//        while (m.find(start)) {
-//            sb.append(m.group(1).toUpperCase());
-//            String rest = m.group(2);
-//            if (rest != null) {
-//                sb.append(rest.toLowerCase());
-//            }
-//            start = m.end();
-//        }
-//        return sb.toString();
-//    }
+    private static final Pattern SNAKE_PATTERN = Pattern.compile("_*([^_])([^_]*)");
+
+    public static String snake2camel(String name) {
+        return snake2camel(name, true);
+    }
+
+    public static String snake2camel(String name, boolean upcaseFirst) {
+        Matcher m = SNAKE_PATTERN.matcher(name);
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        boolean first = true;
+        while (m.find(start)) {
+            if (first) {
+                sb.append(upcaseFirst ?
+                        m.group(1).toUpperCase() :
+                        m.group(1).toLowerCase());
+                first = false;
+            } else {
+                sb.append(m.group(1).toUpperCase());
+            }
+            String rest = m.group(2);
+            if (rest != null) {
+                sb.append(rest.toLowerCase());
+            }
+            start = m.end();
+        }
+        return sb.toString();
+    }
 
     private static String createMethodName(String prefix, String propertyName) {
         String getterName = prefix;
@@ -56,63 +71,75 @@ public class BeanUtil {
         return getterName;
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static void setProperty(Object bean, String name, Object value) {
-        if (bean instanceof Map) {
-            ((Map)bean).put(name, value);
-        } else {
-            try {
-                String setterName = createMethodName("set", name);
-                Method method = bean.getClass().getMethod(setterName, Object.class);
-                method.invoke(bean, value);
-            } catch (Exception e) {
-                LOG.error(DocsMessages.E_9999, e);
+    public static void setValue(Object bean, String name, Object value) throws BeanNotFoundException {
+        Matcher m = BEAN_PATTERN.matcher(name);
+        int start = 0, end = 0;
+
+        while (m.find(start)) {
+            if (m.start() != start) {
+                throw new BeanNotFoundException(DocsMessages.E_5007.getMessage(name));
             }
+
+            // 最後の項目はset
+            if (m.end() == name.length()) {
+                if (m.group(3) == null) {
+                    setProperty(bean, m.group(1), value);
+                } else {
+                    bean = getProperty(bean, m.group(1));
+                    setProperty(bean, m.group(3), value);
+                }
+            } else {
+                bean = getProperty(bean, m.group(1));
+                if (bean == null && end != name.length()) {
+                    throw new BeanNotFoundException(DocsMessages.E_5005.getMessage(name));
+                }
+                if (m.group(3) != null) {
+                    bean = getProperty(bean, m.group(3));
+                    if (bean == null && end != name.length()) {
+                        throw new BeanNotFoundException(DocsMessages.E_5005.getMessage(name));
+                    }
+                }
+            }
+            start = end = m.end();
+        }
+
+        if (start != end) {
+            throw new BeanNotFoundException("指定に誤りがあります：" + name);
         }
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static void setProperty(Object bean, String name, String value) {
-        if (bean instanceof Map) {
-            ((Map)bean).put(name, value);
-        } else {
-            try {
-                String setterName = createMethodName("set", name);
-                Method method = bean.getClass().getMethod(setterName, String.class);
-                method.invoke(bean, value);
-            } catch (Exception e) {
-                LOG.warn(DocsMessages.W_3001, name);
-            }
-        }
-    }
-
-    public static Object getProperty(Object bean, String name) throws BeanNotFoundException {
-        if (bean instanceof Map) {
-            Object result = ((Map<?,?>)bean).get(name);
-            if (result == null) {
-                throw new BeanNotFoundException(name + " from " + bean);
-            }
-            return result;
-        } else {
-            try {
-                String getterName = createMethodName("get", name);
-                Method method = bean.getClass().getMethod(getterName);
-                return method.invoke(bean);
-            } catch (Exception e) {
-                throw new BeanNotFoundException(name + " from " + bean);
-            }
-        }
-    }
-
-    public static void setSnakeProperty(Object bean, String name, Object value) {
-        
+    private static boolean isNumeric(String value) {
+        return value == null ?
+                false :
+                (value.matches("^\\d+$") ? true : false);
     }
 
     public static Object getValue(Object bean, String name) throws BeanNotFoundException {
-        String[] seg = name.split("\\.");
-        for (String key : seg) {
-            bean = getProperty(bean, key);
+        Matcher m = BEAN_PATTERN.matcher(name);
+        int start = 0, end = 0;
+
+        while (m.find(start)) {
+            if (m.start() != start) {
+                throw new BeanNotFoundException(DocsMessages.E_5007.getMessage(name));
+            }
+
+            bean = getProperty(bean, m.group(1));
+            if (bean == null && end != name.length()) {
+                throw new BeanNotFoundException("項目[" + m.group(1) + "]がありません");
+            }
+            if (m.group(3) != null) {
+                bean = getProperty(bean, m.group(3));
+                if (bean == null && end != name.length()) {
+                    throw new BeanNotFoundException("項目[" + m.group(3) + "]がありません");
+                }
+            }
+            start = end = m.end();
         }
+
+        if (start != end) {
+            throw new BeanNotFoundException("指定に誤りがあります：" + name);
+        }
+
         return bean;
     }
 
@@ -120,8 +147,89 @@ public class BeanUtil {
         try {
             return getValue(bean, name);
         } catch (BeanNotFoundException e) {
-            LOG.error(DocsMessages.E_9999, e);
+            LOG.info(DocsMessages.I_0004, name, defaultValue);
             return defaultValue;
+        }
+    }
+
+    public static byte getValueAsByte(Object bean, String name) throws BeanNotFoundException {
+        return ((Byte)getValue(bean, name)).byteValue();
+    }
+    public static short getValueAsShort(Object bean, String name) throws BeanNotFoundException {
+        return ((Short)getValue(bean, name)).shortValue();
+    }
+    public static int getValueAsInt(Object bean, String name) throws BeanNotFoundException {
+        return ((Integer)getValue(bean, name)).intValue();
+    }
+    public static long getValueAsLong(Object bean, String name) throws BeanNotFoundException {
+        return ((Long)getValue(bean, name)).longValue();
+    }
+
+    private static final Pattern BEAN_PATTERN =
+            Pattern.compile("\\.?([^\\.\\[\\s]+)(\\[([^\\]]+)\\])?");
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static void setProperty(Object bean, String name, Object value)
+            throws BeanNotFoundException {
+        if (bean instanceof Map) {
+            ((Map)bean).put(name, value);
+        } else if (bean.getClass().isArray() && isNumeric(name)) {
+            Array.set(bean, Integer.valueOf(name), value);
+        } else if (List.class.isAssignableFrom(bean.getClass())) {
+            ((List)bean).add(Integer.valueOf(name), value);
+        } else {
+            Class<?> targetType = value.getClass();
+            String setterName = createMethodName("set", name);
+            try {
+                Method method = bean.getClass().getMethod(setterName, targetType);
+                method.invoke(bean, value);
+            } catch (Exception e) {
+                if (Long.class.isAssignableFrom(targetType)) {
+                    targetType = byte.class;
+                } else if (Short.class.isAssignableFrom(targetType)) {
+                    targetType = short.class;
+                } else if (Integer.class.isAssignableFrom(targetType)) {
+                    targetType = int.class;
+                } else if (Long.class.isAssignableFrom(targetType)) {
+                    targetType = long.class;
+                } else if (Float.class.isAssignableFrom(targetType)) {
+                    targetType = float.class;
+                } else if (Double.class.isAssignableFrom(targetType)) {
+                    targetType = double.class;
+                }
+                try {
+                    Method method = bean.getClass().getMethod(setterName, targetType);
+                    method.invoke(bean, value);
+                } catch (Exception ex) {
+                    throw new BeanNotFoundException(DocsMessages.E_5005.getMessage(name), ex);
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static Object getProperty(Object bean, String name) throws BeanNotFoundException {
+        if (bean instanceof Map) {
+            Object result = ((Map<?,?>)bean).get(name);
+            return result;
+        } else if (bean.getClass().isArray() && isNumeric(name)) {
+            return Array.get(bean, Integer.valueOf(name));
+        } else if (List.class.isAssignableFrom(bean.getClass())) {
+            return ((List)bean).get(Integer.valueOf(name));
+        } else {
+            try {
+                String getterName = createMethodName("get", name);
+                Method method = bean.getClass().getMethod(getterName);
+                return method.invoke(bean);
+            } catch (Exception e) {
+                try {
+                    Field field = bean.getClass().getDeclaredField(name);
+                    if (!field.isAccessible()) field.setAccessible(true);
+                    return field.get(bean);
+                } catch (Exception ex) {
+                    throw new BeanNotFoundException(DocsMessages.E_5005.getMessage(name), ex);
+                }
+            }
         }
     }
 
--- src/test/java/com/shorindo/docs/BeanUtilTest.java
+++ src/test/java/com/shorindo/docs/BeanUtilTest.java
@@ -17,20 +17,158 @@ package com.shorindo.docs;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
 import com.shorindo.docs.BeanUtil;
+import com.shorindo.docs.BeanUtil.BeanNotFoundException;
 
 /**
  * 
  */
 public class BeanUtilTest {
 
-//    @Test
-//    public void testUnder2Camel() {
-//        assertEquals("Abc", BeanUtil.under2camel("Abc"));
-//        assertEquals("AbcDef", BeanUtil.under2camel("abc_def"));
-//        assertEquals("AbcDefGhi", BeanUtil.under2camel("abc_def__ghi"));
-//    }
+    @Test
+    public void testSnake2Camel() {
+        assertEquals("Abc",       BeanUtil.snake2camel("Abc"));
+        assertEquals("AbcDef",    BeanUtil.snake2camel("_abc_def"));
+        assertEquals("AbcDefGhi", BeanUtil.snake2camel("ABC_DEF__GHI"));
+    }
 
+    @Test
+    public void testGetValue() throws Exception {
+        Object person = createBean();
+        assertEquals("山田太郎",      BeanUtil.getValue(person, "name"));
+        assertEquals(30,              BeanUtil.getValueAsShort(person, "age"));
+        assertEquals(1000000,         BeanUtil.getValueAsInt(person, "income"));
+        assertEquals("山田慎之介",    BeanUtil.getValue(person, "children[1].name"));
+        assertEquals("山田慎之介",    BeanUtil.getValue(person, "children.1.name"));
+        assertEquals("090-XXXX-XXXX", BeanUtil.getValue(person, "phone[mobile]"));
+        assertEquals("090-XXXX-XXXX", BeanUtil.getValue(person, "phone.mobile"));
+    }
+
+    @Test
+    public void testGetValueError() {
+        testGetNameError("value");
+        testGetNameError("children..name");
+        testGetNameError("children[]");
+        testGetNameError("phone.home[123]");
+    }
+    
+    private void testGetNameError(String name) {
+        Object person = createBean();
+        try {
+            BeanUtil.getValue(person, name);
+            fail();
+        } catch (BeanNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSetValue() throws Exception {
+        Object person = createBean();
+        BeanUtil.setValue(person, "name", "山田虎二");
+        assertEquals("山田虎二",      BeanUtil.getValue(person, "name"));
+        BeanUtil.setValue(person, "age", (short)40);
+        assertEquals(40,              BeanUtil.getValueAsShort(person, "age"));
+        BeanUtil.setValue(person, "income", 1200000);
+        assertEquals(1200000,         BeanUtil.getValueAsInt(person, "income"));
+        BeanUtil.setValue(person, "children[1].name", "名無しさん");
+        assertEquals("名無しさん",    BeanUtil.getValue(person, "children[1].name"));
+        BeanUtil.setValue(person, "children[1]", new Person("山田次郎"));
+        assertEquals("山田次郎",    BeanUtil.getValue(person, "children[1].name"));
+        BeanUtil.setValue(person, "phone[mobile]", "090-ZZZZ-ZZZZ");
+        assertEquals("090-ZZZZ-ZZZZ", BeanUtil.getValue(person, "phone[mobile]"));
+        BeanUtil.setValue(person, "phone[mobile]", "090-9999-9999");
+        assertEquals("090-9999-9999", BeanUtil.getValue(person, "phone[mobile]"));
+    }
+
+    private Object createBean() {
+        Person person = new Person("山田太郎");
+        person.setAge((short)30);
+        person.setIncome(1000000);
+        person.setAverage(0.9f);
+        try {
+            person.setBirthday(new SimpleDateFormat("yyyy-MM-dd").parse("1970-01-01"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Person child1 = new Person("山田花子");
+        child1.setAge((short)5);
+        person.getChildren().add(child1);
+
+        Person child2 = new Person("山田慎之介");
+        child2.setAge((short)3);
+        person.getChildren().add(child2);
+
+        person.getPhone().put("mobile", "090-XXXX-XXXX");
+        person.getPhone().put("home", "03-XXXX-XXXX");
+
+        return person;
+    }
+
+    public static class Person {
+        private String name;
+        private short age;
+        private int income;
+        private float average;
+        private Date birthday;
+        private List<Person> children = new ArrayList<Person>();
+        private Map<String,String> phone = new HashMap<String,String>();
+
+        public Person(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public short getAge() {
+            return age;
+        }
+        public void setAge(short age) {
+            this.age = age;
+        }
+        public int getIncome() {
+            return income;
+        }
+        public void setIncome(int income) {
+            this.income = income;
+        }
+        public float getAverage() {
+            return average;
+        }
+        public void setAverage(float average) {
+            this.average = average;
+        }
+        public Date getBirthday() {
+            return birthday;
+        }
+        public void setBirthday(Date birthday) {
+            this.birthday = birthday;
+        }
+        public List<Person> getChildren() {
+            return children;
+        }
+        public void setChildren(List<Person> children) {
+            this.children = children;
+        }
+        public Map<String, String> getPhone() {
+            return phone;
+        }
+        public void setPhone(Map<String, String> phone) {
+            this.phone = phone;
+        }
+    }
 }
